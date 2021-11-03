28e47876934c68781266d349a84c1c2597fb9fc9
yarden hovav <hovav4321@gmail.com>
2021-11-03 14:05:05 +0200

    Adding a play turns images2

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -13,12 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView cell1 = (ImageView) findViewById(R.id.cell_1);
-        ImageView Xcell1 = (ImageView) findViewById(R.id.X_cell_1);
+        ImageView cell1 =findViewById(R.id.cell_1);
+        ImageView Xcell1 =findViewById(R.id.X_cell_1);
+        ImageView Oplay = findViewById(R.id.Oplay);
+        ImageView Xplay = findViewById(R.id.Xplay);
+
+
         cell1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Xcell1.setVisibility(View.VISIBLE);
+                Xplay.setVisibility(View.INVISIBLE);
+                Oplay.setVisibility(View.VISIBLE);
+
             }
         });
     }
