b8b503f0c3f7ea596da14412da6f9aa46d053b65
chenBenTolila <chenbt266@gmail.com>
2021-11-03 13:05:52 +0200

    adding image listener

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("TAG", "My message");
+        ImageView cell1 = (ImageView) findViewById(R.id.cell_1);
+        ImageView Xcell1 = (ImageView) findViewById(R.id.X_cell_1);
+        cell1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Xcell1.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
